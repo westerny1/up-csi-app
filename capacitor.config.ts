75e79d5e8e308b3d0001e0b09c73f4327e3666b4
Benjeouelle Lazaro <balazaro1@up.edu.ph>
2025-03-08 18:38:16 +0800

    feat: add Capacitor.js support (Android & IOS)

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.csiapp.app',
+  appName: 'CSI App',
+  webDir: 'build'
+};
+
+export default config;
