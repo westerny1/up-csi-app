0d730a371b6f6218caef4178a0b822be09aa905e
Benjeouelle Lazaro <balazaro1@up.edu.ph>
2025-03-17 15:53:01 +0800

    Revert "feat: add Capacitor.js support (Android & IOS)"
    This reverts commit 75e79d5e8e308b3d0001e0b09c73f4327e3666b4.

--- android/app/src/main/java/com/csiapp/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.csiapp.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.csiapp.app',
-  appName: 'CSI App',
-  webDir: 'build'
-};
-
-export default config;
