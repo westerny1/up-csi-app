8f07bcd5cbf7f01f3d6a0280bc5441f1c9b235a1
Benjeouelle Lazaro <balazaro1@up.edu.ph>
2025-03-17 16:10:09 +0800

    style: fix format using prettier

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <p>Hello world! (Landing Page)</p>
 
-<a href='/login'> Login </a>
+<a href="/login"> Login </a>
 
-<a href='/consti-quiz'> Constitution Quiz </a>
+<a href="/consti-quiz"> Constitution Quiz </a>
 
-<a href='/sigsheet'> Signature Sheet </a>
+<a href="/sigsheet"> Signature Sheet </a>
