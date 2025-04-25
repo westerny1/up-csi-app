5311dd6f24c0cf3a40265316f127f509963d142e
ligsqt <115415483+LigsQt@users.noreply.github.com>
2025-04-25 16:47:58 +0800

    fix: change let to const

--- src/routes/sigsheet/MemberCard.svelte
+++ src/routes/sigsheet/MemberCard.svelte
@@ -1,15 +1,16 @@
 <script>
-    let { member } = $props();
+   const { member } = $props();
 </script>
 
-<main class="font-[Inter, sans-serif] flex flex-col gap-[0.01rem] rounded-2xl pt-0 px-4 pb-4 bg-[#262629] text-center w-[14vw] hover:bg-[#333336] hover:scale-105 transition-all duration-300 ease-in-out">
+<main
+    class="font-[Inter, sans-serif] flex w-[14vw] flex-col gap-[0.01rem] rounded-2xl bg-[#262629] px-4 pt-0 pb-4 text-center transition-all duration-300 ease-in-out hover:scale-105 hover:bg-[#333336]"
+>
     <div class="mb-[1rem] w-[14vw] self-center">
         <img src={member.photo} class="rounded-2xl" alt={member.name} />
     </div>
 
     <div>
-        <h2 class="text-csi-white text-lg font-bold text-left">{member.name}</h2>
-        <h3 class="text-csi-white text-sm text-left">{member.role}</h3>
+        <h2 class="text-csi-white text-left text-lg font-bold">{member.name}</h2>
+        <h3 class="text-csi-white text-left text-sm">{member.role}</h3>
     </div>
 </main>
-
