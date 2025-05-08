d8e64de366fd84ee38fb4db4cc7f11116a8abba9
rue-22 <cssalces@up.edu.ph>
2025-05-08 10:21:47 +0800

    fix: change to accommodate login feat

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,34 @@
 <script lang="ts">
     import './app.css';
-    import NavBar from './NavBar.svelte';
+    import NavBar from '$lib/NavBar.svelte';
     import { page } from '$app/state';
-    const { children } = $props();
+
+    import { invalidate } from '$app/navigation';
+    import { onMount } from 'svelte';
+    const { data, children } = $props();
+    const { session, supabase } = $derived(data);
+    onMount(() => {
+        const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
+            if (newSession?.expires_at !== session?.expires_at) {
+                invalidate('supabase:auth');
+            }
+        });
+        return () => data.subscription.unsubscribe();
+    });
 </script>
 
-<div class="flex w-full flex-row bg-[#161619]">
-    {#if page.url.pathname !== '/login/'}
-        <div class="flex w-1/6">
-            <NavBar />
-        </div>
-    {/if}
+{#if !data.session}
+    {@render children()}
+{:else}
+    <div class="flex w-full flex-row bg-[#161619]">
+        {#if page.url.pathname !== '/login/'}
+            <div class="flex w-1/6">
+                <NavBar />
+            </div>
+        {/if}
 
-    <div class="flex {page.url.pathname === '/login/' ? 'w-full' : 'w-5/6'}">
-        {@render children()}
+        <div class="flex {page.url.pathname === '/login/' ? 'w-full' : 'w-5/6'}">
+            {@render children()}
+        </div>
     </div>
-</div>
+{/if}
