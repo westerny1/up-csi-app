97ff9acf4d60bb0206a2376ea304eb2c06605ef6
Eriene Galinato <erienegalinato@gmail.com>
2025-05-13 22:23:17 +0800

    fix(ci): use `@sveltejs/adapter-node`

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 /** @import { Config } from '@sveltejs/kit*/
 
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {Config} */
